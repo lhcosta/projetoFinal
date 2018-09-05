93dca9dabc233cff17efdf9e172fbbe21fb94cdc
lhcosta <lucashpc91@gmail.com>
2018-09-05 15:43:58 -0300

    projeto final

--- main.c
+++ main.c
@@ -1,7 +1,26 @@
+/*Síntese - Projeto Final - Lucas Henrique - UC17201125 - Ramón Dias - UC14201004
+Objetivo: Gestão dos testes do Campeonato de Moto GP, auxiliando o usuário com diversas opções de menu
+Entrada: piloto, equipe, circuito, melhor volta
+Saída: de acordo com o menu de opções.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
+#include "menu.h"
 
 int main() {
-	printf("Projeto Final\n");
-	return 0;
+	//Declarações
+	char opcao = '\0';
+	
+	//Instruções
+	
+	telaEntrada();
+	do {
+		apresentaEscolheMenuPrincipal(&opcao);
+		chamaFuncoesMenuPrincipal(&opcao);
+	} while(opcao != 's');
+	
+	return EXIT_SUCCESS;
 }
+
+
---
+++ validacoes.h
@@ -0,0 +1,19 @@
+/*Arquivo de Cabeçalho - Fuções de Validação*/
+#ifndef _VALIDACOES_h
+#define _VALIDACOES_h
+
+#include "gerenciamento.h"
+
+#define TAM_DATA 11
+
+void leValidaFloat(float *pValor, float valorMin, float valorMax, char *texto);
+void leValidaNome(char *texto, char *nome);
+void leValidaChar(char *pChar, char X, char Y, char *titulo);
+void leValidaInt(int *pValor,int valorMin, int valorMax, char *texto);
+void leValidaData(data *data, char *texto);
+void leValidaSigla(char *texto, char *sigla);
+void leValidaTempo(int *minutos, int *segundos, float *milissegundos, char *texto);
+int qtdDiasAniversario(data *dataAniversario);
+int calculaDiaSemana(data *dataVolta);
+
+#endif
